242ae0e0e99440fba93015de2cf8b1e61ef9c86d
fabian1995 <f.hummer@traunseenet.at>
2017-11-17 11:42:50 +0100

    Added copyright notices

--- LabEval/LabEval.m
+++ LabEval/LabEval.m
@@ -1,6 +1,23 @@
 (* Wolfram Language Package *)
 
-(* Created by the Wolfram Workbench Nov 16, 2017 *)
+(*
+	(C) Copyright 2017 Fabian Hummer
+	
+	This file is part of LabEval - Simple Measurement Series Evanualtion Script
+
+	LabEval is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    LabEval is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with LabEval.  If not, see <http://www.gnu.org/licenses/>.
+*)
 
 BeginPackage["LabEval`"]
 (* Exported symbols added here with SymbolName::usage *) 
